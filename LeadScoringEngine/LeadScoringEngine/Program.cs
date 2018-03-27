fd607b6daad45a87de4255b7e99c6e29ec2bd7b8
Jared Fry <jafanders@hotmail.com>
2018-03-27 18:43:17 -0400

    Apply changes

--- LeadScoringEngine/LeadScoringEngine/Program.cs
+++ LeadScoringEngine/LeadScoringEngine/Program.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using LeadScoringEngine.Display;
+using LeadScoringEngine.FileLoading;
+
 namespace LeadScoringEngine
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-
+            
         }
     }
 }
---
+++ LeadScoringEngine/LeadScoringEngine/SalesLead.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeadScoringEngine.FileLoading;
+
+namespace LeadScoringEngine
+{
+    public class SalesLead : Serializable
+    {
+        public int Id { get; set; }
+        public string Event { get; set; }
+        public decimal Score { get; set; }
+
+        public override Serializable Deserialize(string fileLine)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
